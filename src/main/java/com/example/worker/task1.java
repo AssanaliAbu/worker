8d1afced400deff0759a2030f74fb37f3f7af25b
Assanali Abu <assanali.abu@nu.edu.kz>
2021-06-25 16:05:33 +0600

    e

--- src/main/java/com/example/worker/task1.java
+++ src/main/java/com/example/worker/task1.java
@@ -7,6 +7,7 @@ import org.camunda.bpm.client.task.ExternalTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Component;
+//import com.example.commonlibs.CamundaFeign;
 //import com.example.library.Feign;
 
 import java.util.HashMap;
@@ -17,8 +18,8 @@ import java.util.Map;
 @Component
 @ExternalTaskSubscription(topicName = "test-topic-name1")
 public class task1 implements ExternalTaskHandler {
-    @Autowired
-//    Feign feign;
+//    @Autowired
+//    CamundaFeign feign;
     public static String position;
     public static String name;
     @Autowired
